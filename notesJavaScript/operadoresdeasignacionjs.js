6b76fa7303fa2f63b1f34883e7915ddc876aeca2
JCode <jhsl.personal@gmail.com>
2023-03-19 15:21:53 -0500

    update

---
+++ notesJavaScript/operadoresdeasignacionjs.js
@@ -0,0 +1,13 @@
+var a;
+var b = 2;
+
+console.log(a); // <- La variable "a" NO tiene un valor asignado
+//Resultado es: undefined
+console.log(b); // <- La variable "b" SI tiene un valor asignado
+//Resultado es: 2
+
+// = es el operador de asignacion en JavaScript
+
+a = 10;
+
+console.log(a); // <-- La variable "a" YA cuenta con valor asignado
