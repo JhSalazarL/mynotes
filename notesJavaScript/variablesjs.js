a193a07c285fafdf91e8d11b095c12bcac97cc80
JCode <jhsl.personal@gmail.com>
2023-03-19 15:38:12 -0500

    update

--- notesJavaScript/variablesjs.js
+++ notesJavaScript/variablesjs.js
@@ -1,16 +1,23 @@
 /*
 Tipos de Datos:
 
-En informática, los datos son cualquier cosa que tenga sentido para la computadora. JavaScript proporciona ocho tipos de datos diferentes, los cuales son undefined, null, boolean, string, symbol, bigint, number, y object.
+En informática, los datos son cualquier cosa que tenga sentido
+para la computadora. 
+JavaScript proporciona ocho tipos de datos diferentes, 
+los cuales son undefined, null, boolean, string, symbol, bigint, 
+number, y object.
 */
 
+                    String
 var miVariable = "freeCodeCamp";
 
-
 console.log(miVariable);
+// "freeCodeCamp"
 
 miVariable = 16;
 console.log(miVariable);
+// "16"
 
 var miNombre = "Salazar";
 console.log(miNombre);
+// "Salazar"
